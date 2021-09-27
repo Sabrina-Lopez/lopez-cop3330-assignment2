bce6ec8e50c1be3805f362c30c4ffcf26a9618c7
Berrybunny015 <berrybunny015@gmail.com>
2021-09-26 21:32:14 -0400

    completed exercise 28 along w/ text-based flowchart

--- src/main/java/ex28/App.java
+++ src/main/java/ex28/App.java
@@ -9,8 +9,28 @@ import java.util.Scanner;
 
 public class App {
     public static void main( String[] args ) {
+        int sumResult = 0; //result of the summation
 
+        for(int i = 0; i < 5; i++) {
+            System.out.println("\nEnter a number: "); //ask the user for a number
+            Scanner numberInput = new Scanner(System.in); //scan their number
+            int number = numberInput.nextInt();
 
+            sumResult = sumResult + number;
+        }
+
+        System.out.println("\nThe total is " + sumResult + ".");
 
     }
 }
+
+/* Flowchart:
+
+                                                                               (if "i" = 5) --> end the for loop -> print out the summation
+                                                                                     |
+    ask user for number -> scan the number -> add the number to the summation -> increment "i" --(if "i" < 5)--
+    ^                                                                                                         |
+    |                                                                                                         |
+    -----------------------------------------------------------------------------------------------------------
+
+ */

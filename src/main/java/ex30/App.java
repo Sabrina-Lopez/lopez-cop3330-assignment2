64f7675fc683158b2e1059a4953ed56faf0cf0ec
Berrybunny015 <berrybunny015@gmail.com>
2021-09-27 00:33:25 -0400

    completed exercise 30

--- src/main/java/ex30/App.java
+++ src/main/java/ex30/App.java
@@ -5,12 +5,18 @@ package ex30;
  *  Copyright 2021 first_name last_name
  */
 
-import java.util.Scanner;
-
 public class App {
     public static void main( String[] args ) {
+        int currNum;
 
-
+        for(int i = 1; i < 13; i++) { //loop to go through the 12 values
+            System.out.println("\n"); //move to next row of the multiplication table
+            currNum = 0; //reset to 0
+            for (int j = 0; j < 12; j++) { //loop go to through the 12 multiples of each value
+                currNum = currNum + i; //increment to next multiple
+                System.out.printf("%-5d", currNum); //print the next number in the multiplication table
+            }
+        }
 
     }
 }

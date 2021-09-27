2db7f9d766594c165476d57a524c6a16a191b9e6
Berrybunny015 <berrybunny015@gmail.com>
2021-09-27 07:55:22 -0400

    completed exercise 33

--- src/main/java/ex33/App.java
+++ src/main/java/ex33/App.java
@@ -10,7 +10,27 @@ import java.util.Scanner;
 public class App {
     public static void main( String[] args ) {
 
+        System.out.println("\nWhat is your question? "); //ask the user's question
+        System.out.print("> ");
+        Scanner questionInput = new Scanner(System.in); //scan their question
+        String question = questionInput.nextLine();
 
+        int randomChoice = (int)Math.floor(Math.random() * (4 - 1 + 1) + 1);
+
+        switch (randomChoice) {
+            case 1:
+                System.out.println("\nYes. ");
+                break;
+            case 2:
+                System.out.println("\nNo. ");
+                break;
+            case 3:
+                System.out.println("\nMaybe. ");
+                break;
+            case 4:
+                System.out.println("\nAsk again later. ");
+                break;
+        }
 
     }
 }
